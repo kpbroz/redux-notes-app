06ba84b13921dea88fb696e1f9387a0113261959
kp_broz <abhimanyukudla@gmail.com>
2024-02-19 18:59:17 +0530

    App files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Todos from "./components/Todos";
+import AddToDo from "./components/AddToDo";
+
+function App() {
+  return (
+    <div className="App">
+      <AddToDo />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
